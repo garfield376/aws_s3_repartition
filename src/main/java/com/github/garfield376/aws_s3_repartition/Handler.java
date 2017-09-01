753ee53b1b33d314ef02e3525e0a83cd7ae49792
Fei Lang <faye1108@gmail.com>
2017-08-31 21:24:18 -0700

    Implementing aws_s3_repartition

---
+++ src/main/java/com/github/garfield376/aws_s3_repartition/Handler.java
@@ -0,0 +1,13 @@
+package com.github.garfield376.aws_s3_repartition;
+
+/**
+ * Hello world!
+ *
+ */
+public class Handler 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
