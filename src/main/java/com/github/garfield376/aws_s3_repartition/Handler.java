46be49f7aab78860c2139b129157171db463d963
Fei Lang <faye1108@gmail.com>
2017-09-02 22:27:06 -0700

    Fixing a bug when using a prefix in the source S3 bucket it will create a destination key with /prefix at the end.

--- src/main/java/com/github/garfield376/aws_s3_repartition/Handler.java
+++ src/main/java/com/github/garfield376/aws_s3_repartition/Handler.java
@@ -13,7 +13,8 @@ public class Handler
 {
 	private AmazonS3 s3;
 	private String destinationBucketName = System.getenv("dest_bucket");   //"dest_bucket" is one environment key set at Lambda runtime
-
+    private String destinationPrefix = System.getenv("dest_prefix"); //"dest_key" is one environment key set at Lambda runtime
+    
 	public Handler() {
 		s3 = AmazonS3ClientBuilder
 		          .standard()
@@ -35,16 +36,18 @@ public class Handler
 	    // Rename as follows:
 	    //
 	    //   bucket:  cfsrc                                          -> cfdest
-	    //   key:     prefix/EP4CMY7FDJR6Y.2017-06-28-16.ea6de06c.gz -> yyyy=2017/mm=06/dd=28/hh=16/EP4CMY7FDJR6Y.2017-06-28-16.ea6de06c.gz
+	    //   key:     prefix/EP4CMY7FDJR6Y.2017-06-28-16.ea6de06c.gz -> destprefix/yyyy=2017/mm=06/dd=28/hh=16/EP4CMY7FDJR6Y.2017-06-28-16.ea6de06c.gz
 	    //
 	    
-	    String regex   = "(?<distributionid>.*)\\.(?<year>[0-9]{4})\\-(?<month>[0-9]{2})\\-(?<day>[0-9]{2})\\-(?<hour>[0-9]{2})\\.(?<hash>.*)\\.gz";
+	    String regex   = "(?<prefix>.*)\\/(?<distributionid>.*)\\.(?<year>[0-9]{4})\\-(?<month>[0-9]{2})\\-(?<day>[0-9]{2})\\-(?<hour>[0-9]{2})\\.(?<hash>.*)\\.gz";
 	    String replace = "yyyy=${year}/mm=${month}/dd=${day}/hh=${hour}/${distributionid}.${year}-${month}-${day}-${hour}.${hash}.gz";
-	    String destinationKey = sourceKey.replaceAll(regex, replace);
+	    String destinationKey = destinationPrefix +"/" +sourceKey.replaceAll(regex, replace);
 
 	    try {
 	      s3.copyObject(sourceBucketName, sourceKey, 
 	        destinationBucketName, destinationKey);
+	     
+	      
 	    } catch (AmazonClientException ex) {
 	      System.out.println(ex.toString());
 	    }
